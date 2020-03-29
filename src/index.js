b255f620ae9cbdbb7a2044d512c404eb9887409b
tilak.jayanna <tilak.jayanna@epicor.com>
2020-03-29 18:43:01 +0530

    Changed theme color in manifest.json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'animate.css'
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 
 
@@ -13,5 +14,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
 
 
